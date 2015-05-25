3ad05d0d1200042201c3d301084e61ba70e516e4
denisx <denisx@denisx.ru>
2015-05-25 23:15:57 +0300

    add

--- f/youtube.js
+++ f/youtube.js
@@ -2,7 +2,7 @@ $(function(){
 	$('a').each(function(){
 		var href = $(this).attr('href') + '&output=embed';
 		if ( /https?:\/\/(www.)?youtube.com/.test(href) ){
-			var iframe = $('<div class="video youtube"><iframe width="425" height="349" src="' + href + '" frameborder="0" allowfullscreen></iframe></div>');
+			var iframe = $('<div class="video youtube"><iframe width="600" height="400" src="' + href + '" frameborder="0" allowfullscreen="true"></iframe></div>');
 			var item = $(this);
 			iframe.insertAfter(item);
 			item.remove();
