414dbbcc2e4c4fc5dc6811537d2f1175940bb223
denisx <denisx@denisx.ru>
2015-05-25 23:07:15 +0300

    add

---
+++ f/youtube.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('a').each(function(){
+		var href = $(this).attr('href');
+		if ( /https?:\/\/(www.)?youtube.com/.test(href) ){
+			var iframe = $('<div class="video youtube"><iframe width="425" height="349" src="' + href + '" frameborder="0" allowfullscreen></iframe></div>');
+			var item = $(this);
+			iframe.insertAfter(item);
+			item.remove();
+		}
+	});
+});
